56d95919377b698947b17e0cecfb3aaf511b8ddf
Tim Ermilov <yamalight@gmail.com>
2018-12-11 18:09:46 +0100

    Refactor code, prepare to open source it

---
+++ src/api.js
@@ -0,0 +1,104 @@
+const got = require('got');
+const dateFns = require('date-fns');
+
+// Offical Warframe API endpoint, essentially a really big JSON dump
+const dataUrl = 'http://content.warframe.com/dynamic/worldState.php';
+
+/**
+ * Items of interest in invasions
+ * Uses item substring after last `/`
+ * (in theory it might be trigger on false items at one point,
+ * but so far works fine)
+ */
+const interestingItems = [
+  'OrokinReactorBlueprint',
+  'OrokinCatalystBlueprint',
+  'PlayerMeleeWeaponRandomModRare',
+  'LotusPistolRandomModRare',
+  'LotusRifleRandomModRare',
+  'LotusShotgunRandomModRare',
+];
+
+/**
+ * Items of interest in alerts
+ * Follows the same pattern as invasion items
+ */
+const interestingItemsAlerts = interestingItems.concat(['FormaBlueprint', 'Alertium', 'CatbrowGeneticSignature']);
+
+/**
+ * Mappings between item values and human-readable names
+ */
+const itemNames = {
+  OrokinReactorBlueprint: 'Orokin Reactor Blueprint',
+  OrokinCatalystBlueprint: 'Orokin Catalyst Blueprint',
+  Alertium: 'Nitain',
+  FormaBlueprint: 'Forma Blueprint',
+  PlayerMeleeWeaponRandomModRare: 'Melee Riven Mod',
+  LotusPistolRandomModRare: 'Pistol Riven Mod',
+  LotusRifleRandomModRare: 'Rifle Riven Mod',
+  LotusShotgunRandomModRare: 'Shotgun Riven Mod',
+  CatbrowGeneticSignature: 'Kavat Genetic Code',
+};
+
+/**
+ * Fetches data from warframe API and returns filtered,
+ * simply formatted arrays of invasions and alerts
+ * that include interesting items
+ */
+exports.fetchData = async () => {
+  const response = await got(dataUrl, {json: true});
+  const {Alerts, Invasions} = response.body;
+
+  // parse alerts
+  const now = Date.now();
+  const alerts = Alerts.map(a => ({
+    id: a._id.$oid,
+    start: new Date(Number(a.Activation.$date.$numberLong)),
+    end: new Date(Number(a.Expiry.$date.$numberLong)),
+    type: a.MissionInfo.missionType,
+    rewards: (a.MissionInfo.missionReward.items || [])
+      .concat((a.MissionInfo.missionReward.countedItems || []).map(it => it.ItemType))
+      .map(reward => reward.split('/').pop()),
+  }))
+    .filter(a => a.end.getTime() > now)
+    .filter(alert => alert.rewards.some(reward => interestingItemsAlerts.includes(reward)));
+
+  // parse invasions
+  const invasions = Invasions.filter(i => !i.Completed)
+    .map(i => ({
+      id: i._id.$oid,
+      count: i.Count,
+      goal: i.Goal,
+      attackReward: (i.AttackerReward.countedItems || []).map(it => it.ItemType.split('/').pop()),
+      defenderReward: (i.DefenderReward.countedItems || []).map(it => it.ItemType.split('/').pop()),
+    }))
+    .filter(invasion =>
+      invasion.attackReward.concat(invasion.defenderReward).some(reward => interestingItems.includes(reward))
+    );
+
+  return {alerts, invasions};
+};
+
+/**
+ * Formats given alert into a string
+ * @param {Object} alert
+ */
+exports.formatAlert = alert => `NEW ALERT:
+Rewards: ${alert.rewards
+  .map(reward => itemNames[reward])
+  .filter(r => r)
+  .join(' ')}
+Ends in: ${dateFns.differenceInMinutes(alert.end, now)} mins
+${alert.start > now ? `Starts in: ${dateFns.differenceInMinutes(alert.start, now)} mins` : ''}`;
+
+/**
+ * Formats given invasion into a string
+ * @param {Object} invasion
+ */
+exports.formatInvasion = invasion => `NEW INVASION: 
+Rewards: ${invasion.attackReward
+  .concat(invasion.defenderReward)
+  .map(reward => itemNames[reward])
+  .filter(r => r)
+  .join(', ')}
+Current progress: ~${(Math.floor(Math.abs(invasion.count / invasion.goal) * 100) / 100) * 100}%`;
---
+++ src/index.js
@@ -0,0 +1,110 @@
+const dateFns = require('date-fns');
+const Telegraf = require('telegraf');
+const Cache = require('ttl-mem-cache');
+const {fetchData, formatAlert, formatInvasion} = require('./api');
+
+// Interval to re-fetch the data
+const CHECK_INTERVAL = 10 * 60 * 1000; // 10 mins
+// Time-to-live for invasion data
+const INVASION_TTL = 12 * 60 * 60 * 1000; // 12 hours
+// cache for already mentioned things
+const cache = new Cache();
+
+/**
+ *
+ * @param {*} ctx
+ */
+const handleCheck = async ctx => {
+  const {alerts, invasions} = await fetchData();
+
+  // send alert messages
+  const now = Date.now();
+  alerts
+    // filter alert that were already sent to chat
+    .filter(alert => !cache.get(alert.id))
+    .forEach(alert => {
+      // add to cache
+      cache.set(alert.id, alert.id, dateFns.differenceInMilliseconds(alert.end, now));
+      // reply with formatted human-readable info about alert
+      ctx.reply(formatAlert(alert));
+    });
+
+  // send invasion messages
+  invasions
+    // filter invasions that were already sent to chat
+    .filter(invasion => !cache.get(invasion.id))
+    .forEach(invasion => {
+      // add to cache
+      cache.set(invasion.id, invasion.id, INVASION_TTL);
+      // reply with formatted human-readable info about invasion
+      ctx.reply(formatInvasion(invasion));
+    });
+};
+
+/**
+ * Main function that starts the bot.
+ * The bot will only work in one group / chat as it is now
+ */
+module.exports = () => {
+  // bot status
+  let status = 'waiting';
+  // re-fetch interval reference
+  let nextCheckInterval = -1;
+  // default context that is passed to handler
+  let context = {
+    // by default reply will log data to console
+    reply: (...args) => console.log(args),
+  };
+
+  // telegraf bot instance
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+
+  // start command
+  // has to be invoke for bot to start handling alerts
+  bot.command('lwstart', async ctx => {
+    // ignore multiple calls
+    if (status === 'running') {
+      ctx.reply('Already running!');
+      return;
+    }
+
+    // notify user that we're starting
+    ctx.reply(`OK, I'll let you know when cool things are in alerts or invasions.`);
+    // set new status
+    status = 'running';
+    // store chat context for replies
+    context = ctx;
+    // call initial alerts parsing
+    await handleCheck(ctx);
+    // schedule checks at the given interval
+    nextCheckInterval = setInterval(() => handleCheck(ctx), CHECK_INTERVAL);
+  });
+
+  // status command
+  // reports current bot status
+  bot.command('lwstatus', ctx => ctx.reply(`Current status: ${status}`));
+
+  // stop command
+  // stops current alert monitoring, cleans up refresh interval
+  bot.command('lwstop', ctx => {
+    status = 'stopped';
+    ctx.reply('Will no longer report alerts!');
+    if (nextCheckInterval) {
+      clearInterval(nextCheckInterval);
+    }
+  });
+
+  // help command
+  // displays available commands to user
+  bot.command('lwhelp', ctx => {
+    ctx.reply(`Here's commands I know:
+* /lwstart - will start monitoring process
+* /lwstop - will stop monitoring process
+* /lwstatus - will display current monitoring status
+* /lwhelp - this help thingy`);
+  });
+
+  // start polling chat for messages
+  bot.startPolling();
+  console.log('Bot is now running!');
+};
